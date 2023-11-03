24a8249d6a4a0618ee18c9efea3b00ae75c56045
weijunnlim <127861973+weijunnlim@users.noreply.github.com>
2023-11-04 02:12:40 +0800

    Camp.java

--- Camp.java
+++ Camp.java
@@ -6,7 +6,7 @@ public class Camp {
     private String campName;
     private LocalDate campDate;
     private LocalDateTime closingTime;
-    private String userGroup;
+    private Faculty userGroup;
     private String location;
 
     private int totalSlots;
@@ -15,7 +15,74 @@ public class Camp {
 
     private String description;
     private User staffInCharge;
-    private ArrayList<Student> listOfStudents = new ArrayList<Student>(totalSlots);
+    private ArrayList<Student> listOfStudents = new ArrayList<Student>(totalSlots);'
+
+    public Camp(String campName, LocalDate campDate, LocalDateTime closingTime, Faculty userGroup, String location, int totalSlots, int campComSlots, int slotsLeft, User staffInCharge) {
+        this.campName = campName;
+        this.campDate = campDate;
+        this.closingTime = closingTime;
+        this.userGroup = userGroup;
+        this.location = location;
+        this.totalSlots = totalSlots;
+        this.campComSlots = campComSlots;
+        this.staffInCharge = staffInCharge;
+        this.slotsLeft = slotsLeft;
+    }
+
+    public String getCampName() {
+        return this.campName;
+    }
+    public void setCampName(String campName) {
+        this.campName = campName;
+    }
+    public LocalDate getCampDate() {
+        return this.campDate;
+    }
+    public void setCampDate(LocalDate campDate) {
+        this.campDate = campDate;
+    }
+    public LocalDateTime getClosingTime() {
+        return this.closingTime;
+    }
+    public void setClosingTime(LocalDateTime closingTime) {
+        this.closingTime = closingTime;
+    }
+    public Faculty getUserGroup() {
+        return this.userGroup;
+    }
+    public void setUserGroup(Faculty userGroup) {
+        this.userGroup = userGroup;
+    }
+    public String getLocation() {
+        return this.location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+     public int getTotalSlots() {
+        return this.totalSlots;
+     }
+     public void setTotalSlots(int totalSlots) {
+        this.totalSlots = totalSlots;
+     }
+    public int getCampComSlots() {
+        return this.campComSlots;
+    }
+    public void setCampComSlots(int campComSlots) {
+        this.campComSlots = campComSlots;
+    }
+    public int getSlotsLeft(){
+        return this.slotsLeft;
+    }
+    public void setSlotsLeft(int slotsLeft){ //i dont think this will be needed? just total-campcomslots
+        this.slotsLeft = slotsLeft;
+    }
+    public User getStaffInCharge() {
+         return staffInCharge;
+    }
+    public void setStaffInCharge(Staff staffInCharge) {
+         this.staffInCharge = staffInCharge;
+    }
 
     public void viewListStudents(){
         //return the arraylist of students
