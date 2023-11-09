5a78d8ad72e05f3b82562063b48cbb3543ce30d2
weijunnlim <127861973+weijunnlim@users.noreply.github.com>
2023-11-09 14:49:20 +0800

    Staff.java

--- Staff.java
+++ Staff.java
@@ -1,12 +1,25 @@
 package Java.SC2002FinalProj;
 import java.util.*;
+import java.util.Scanner;
 
 public class Staff extends User{
     private ArrayList<Camp> campsCreated = new ArrayList<Camp>(10);
 
     public Staff(){
         super();
+        this.campsCreated = new ArrayList<Camp>();
     }
+
+    public void createCamp(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter camp name: ");
+        String campName = sc.nextLine();
+        Camp camp1 = new Camp(campName, this.getUserID());
+        campsCreated.add(camp1);
+        System.out.println("Camp created successfully!");
+        sc.close();
+    }
+
 
     public void viewCampList(){
     }
