a4017330c123444e7f74be8f1257589c1e6732f6
brandonliang1241 <108062128+brandonliang1241@users.noreply.github.com>
2023-11-10 14:31:27 +0800

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,9 +1,12 @@
-package Java.SC2002FinalProj;
+package excel;
 
 public class Student extends User implements CampCom{
     private boolean isCampCom;
     public void viewCampList(){}
     
+    public Student(String userId, String password, Faculty facultyInfo){
+        super(userId, password, facultyInfo);
+    }
     //all below use if isCamCom is True.
     public void viewCampDetails(){}
     public void submitSuggestions(){}
