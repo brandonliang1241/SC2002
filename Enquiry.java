5bdf11ccf59e0bb2b4d509348ff35e62eb0f3f8a
weijunnlim <127861973+weijunnlim@users.noreply.github.com>
2023-11-12 00:13:19 +0800

    Update Enquiry.java

--- Enquiry.java
+++ Enquiry.java
@@ -1,17 +1,16 @@
 package excel;
+
 public class Enquiry {
     private String enquiryText;
     private String status; // e.g., "Pending", "Resolved", "In Progress"
     private String owner; // ID or name of the user who made the enquiry
 
-    // Constructor
     public Enquiry(String enquiryText, String owner) {
         this.enquiryText = enquiryText;
         this.owner = owner;
         this.status = "Pending"; // Default status
     }
 
-    // Getters and Setters
     public String getEnquiryText() {
         return enquiryText;
     }
@@ -36,7 +35,6 @@ public class Enquiry {
         this.owner = owner;
     }
 
-    // Method to display enquiry details
     public void displayEnquiry() {
         System.out.println("Enquiry by: " + owner);
         System.out.println("Status: " + status);
