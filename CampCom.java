924c3ccf7b8901daff82c4ba777457c03ab2aeea
weijunnlim <127861973+weijunnlim@users.noreply.github.com>
2023-11-14 16:58:25 +0800

    Update CampCom.java

--- CampCom.java
+++ CampCom.java
@@ -2,6 +2,8 @@ package Java.SC2002FinalProj;
 import java.util.*;
 
 public interface CampCom {
+    private int points = 0;
+
     public void viewCampDetails();
     public void submitSuggestions();
     public void viewEnquiries();
