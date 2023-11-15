41b37a3a7f156b68702a8b3c8472d3a14caf2c20
weijunnlim <127861973+weijunnlim@users.noreply.github.com>
2023-11-15 11:49:46 +0800

    Update Suggestions.java

--- Suggestions.java
+++ Suggestions.java
@@ -2,4 +2,9 @@ package excel;
 
 public class Suggestion {
   private String suggestionText;
-  private boolean
+  private boolean status; //accepted or rejected
+
+  public Suggestion(String suggestionText){
+    this.suggestionText = suggestionText;
+    status = false;
+  }
