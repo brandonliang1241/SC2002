353eb7d32006cd4b9538271141de6c1425d28daa
weijunnlim <127861973+weijunnlim@users.noreply.github.com>
2023-11-17 15:08:52 +0800

    Update Enquiry.java

--- Enquiry.java
+++ Enquiry.java
@@ -1,15 +1,20 @@
 package excel;
 
 public class Enquiry {
+	private static int enquiryCounter = 0;
+	
     private String enquiryText;
-    private String status; // e.g., "Pending", "Resolved", "In Progress"
+    private boolean status; //true = replied, false = not replied
+    private int enquiryIndex;
     private String owner; // ID or name of the user who made the enquiry
     private String campName;
 
     public Enquiry(String enquiryText, String owner, String campName) {
         this.enquiryText = enquiryText;
+        this.enquiryIndex = enquiryCounter;
+        enquiryCounter++;
         this.owner = owner;
-        this.status = "Pending"; // Default status
+        this.status = false; // Default status not replied yet
         this.campName = campName;
     }
 
@@ -21,12 +26,20 @@ public class Enquiry {
         this.enquiryText = enquiryText;
     }
 
-    public String getStatus() {
+    public boolean getStatus() {
         return status;
     }
 
-    public void setStatus(String status) {
+    public void setStatus(boolean status) {
         this.status = status;
+    }
+    
+    public int getEnquiryIndex() {
+    	return enquiryIndex;
+    }
+    
+    public void setEnquiryIndex(int enquiryIndex) {
+    	this.enquiryIndex = enquiryIndex;
     }
 
     public String getOwner() {
@@ -46,8 +59,7 @@ public class Enquiry {
     }
 
     public void displayEnquiry() {
-        System.out.println("Enquiry by: " + owner);
+        System.out.println("Enquiry: " + enquiryText + " by " + owner);
         System.out.println("Status: " + status);
-        System.out.println("Enquiry: " + enquiryText);
     }
 }
