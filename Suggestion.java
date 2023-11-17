e0a9e79ef41a69331ed3c301dd07695914569117
weijunnlim <127861973+weijunnlim@users.noreply.github.com>
2023-11-17 14:41:09 +0800

    Update Suggestion.java

--- Suggestion.java
+++ Suggestion.java
@@ -4,14 +4,18 @@ public class Suggestion {
   private static int suggestionCounter = 0;
 	
   private String suggestionText;
-  private int suggestionIndex;
   private boolean status; //accepted or rejected
+  private int suggestionIndex;
+  private String owner; // ID or name of the user who made the enquiry
+  private String campName;
 
-  public Suggestion(String suggestionText){
+  public Suggestion(String suggestionText, String owner, String campName){
     this.suggestionText = suggestionText;
     this.suggestionIndex = suggestionCounter;
     suggestionCounter++;
     status = false;  //initialise suggestion as not accepted
+    this.owner = owner;
+    this.campName = campName;
   }
 
   public String getSuggestionText() {
@@ -37,9 +41,31 @@ public class Suggestion {
   public void setStatus(boolean status){
     this.status = status;
   }
+  
+  public String getOwner() {
+	  return owner;
+  }
+  
+  public void setOwner(String owner) {
+	  this.owner = owner;
+  }
+  
+  public String getCampName() {
+	  return campName;
+  }
 
+  public void setCampName(String campName) {
+	  this.campName = campName;
+  }
+  
   public String toString() {
       return "Suggestion: " + suggestionText + "\nStatus: " + (status ? "Accepted" : "Rejected");
+  }
+  
+  public void displaySuggestion() {
+      System.out.println("Suggestion Index: " + suggestionIndex);
+      System.out.println("Suggestion: " + suggestionText + " by " + owner);
+      System.out.println("Status: " + status);
   }
 }
     
