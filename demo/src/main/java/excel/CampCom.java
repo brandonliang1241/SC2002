d4689328c298f82883912e076b43761461a10117
weijunnlim <127861973+weijunnlim@users.noreply.github.com>
2023-11-24 17:40:21 +0800

    Update CampCom.java

--- demo/src/main/java/excel/CampCom.java
+++ demo/src/main/java/excel/CampCom.java
@@ -1,5 +1,6 @@
 package excel;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CampCom {
@@ -34,10 +35,85 @@ public class CampCom {
         this.camp = camp;
     }
 
-    public void viewCampDetails(Camp camp){};
+    public void viewCampDetails(Camp camp){
+        if (camp == null) {
+            System.out.println("No camp details available.");
+            return;
+        }
+        System.out.println("Camp Name: " + camp.getCampName());
+        System.out.println("Camp Date: " + camp.getCampDate());
+        System.out.println("Closing Time: " + camp.getClosingTime());
+        System.out.println("Location: " + camp.getLocation());
+        System.out.println("Total Slots: " + camp.getTotalSlots());
+        System.out.println("Camp Committee Slots: " + camp.getCampComSlots());
+        System.out.println("User Group: " + camp.getUserGroup());
+        System.out.println("Description: " + camp.getDescription());
+        // available for extension for other details
+    }
+    
+    public void viewAllEnquiries(Camp camp){
+        if (camp == null) {
+            System.out.println("No camp details available.");
+            return;
+        }
+
+        ArrayList<Enquiry> enquiries = camp.getEnquiries();
+        if (enquiries.isEmpty()) {
+            System.out.println("No enquiries available for camp: " + camp.getCampName());
+        } else {
+            System.out.println("Enquiries for camp: " + camp.getCampName());
+            for (Enquiry enquiry : enquiries) {
+                enquiry.displayEnquiry();
+            }
+        }
+    }
+    
+    public void replyEnquiries(Scanner sc, Camp camp){
+        if (camp == null) {
+            System.out.println("No camp details available.");
+            return;
+        }
+
+        ArrayList<Enquiry> enquiries = camp.getEnquiries();
+        if (enquiries.isEmpty()) {
+            System.out.println("No enquiries available for camp: " + camp.getCampName());
+            return;
+        }
+
+        System.out.println("Enquiries for camp: " + camp.getCampName());
+        for (int i = 0; i < enquiries.size(); i++) {
+            enquiries.get(i).displayEnquiry();
+        }
+
+        System.out.println("Enter the number of the enquiry you wish to reply to:");
+        int enquiryNumber;
+        try {
+            enquiryNumber = Integer.parseInt(sc.nextLine());
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input. Please enter a number.");
+            return;
+        }
+
+        if (enquiryNumber < 1 || enquiryNumber > enquiries.size()) {
+            System.out.println("Invalid enquiry number.");
+            return;
+        }
+
+        Enquiry selectedEnquiry = enquiries.get(enquiryNumber - 1);
+
+        System.out.println("Enter your reply:");
+        String reply = sc.nextLine();
+
+        selectedEnquiry.setReplyText(reply);
+        selectedEnquiry.setStatus(true); // Set the status as replied
+        this.points++;
+
+        System.out.println("Reply sent successfully.");
+        System.out.println("Updated Enquiry:");
+        selectedEnquiry.displayEnquiry();
+    }
+    
     public void submitSuggestions(Camp camp){};
-    public void viewAllEnquiries(Camp camp){};
-    public void replyEnquiries(Camp camp){};
 
     public void addPoints(Scanner sc){
         int choice;
